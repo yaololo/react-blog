a98cacb060c2e4bb372ed822defdd202498d2e2e
zhuangyongming <yongming.zhuang@moneysmart.com>
2019-01-23 12:13:51 +0800

    Create build

---
+++ client/build/precache-manifest.449c05d177cd13edb88e2923440bf402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29a92e8ac2304b894c0c",
+    "url": "/static/js/main.29a92e8a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "29a92e8ac2304b894c0c",
+    "url": "/static/css/main.fca74b65.chunk.css"
+  },
+  {
+    "revision": "0005a20c711a20e49f1c250533fb7345",
+    "url": "/index.html"
+  }
+];
