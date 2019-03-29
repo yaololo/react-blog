afebeb47b4673ef4c0205a126ff0d85cfd488696
zhuangyongming <yongming.zhuang@moneysmart.com>
2019-03-29 17:17:15 +0800

    Serve frontend static build

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 app.use(bodyParser.json());
+app.use("/static", express.static(path.join(__dirname, "../client/build")));
 
-app.use('/', (req, res) => {
+// if it does not match any route, return the default html file
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+app.use("/", (req, res) => {
   res.send({ data: "hello world" });
 });
+
 
 const server = app.listen(process.env.port || 3001, () => {
   console.log(`App is listening on port: ${server.address().port}`);
