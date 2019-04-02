87d33526bcccfd272e9c4fd84b7e6b599596a902
zhuangyongming <yongming.zhuang@moneysmart.com>
2019-04-02 13:02:42 +0800

    User cors in express

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/static", express.static(path.join(__dirname, "../client/build")));
 
@@ -14,7 +16,6 @@ app.get("/*", function(req, res) {
 app.use("/", (req, res) => {
   res.send({ data: "hello world" });
 });
-
 
 const server = app.listen(process.env.port || 3001, () => {
   console.log(`App is listening on port: ${server.address().port}`);
