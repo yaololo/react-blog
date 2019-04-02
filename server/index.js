5872744c221fd4606a7f14471b430cc2ae46742c
zhuangyongming <yongming.zhuang@moneysmart.com>
2019-04-02 15:05:23 +0800

    Chnage the order of route handler, the fallback handler should be the least handler

--- server/index.js
+++ server/index.js
@@ -8,13 +8,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/static", express.static(path.join(__dirname, "../client/build")));
 
+app.use("/api", (req, res) => {
+  res.send({ data: "hello world" });
+});
+
 // if it does not match any route, return the default html file
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-
-app.use("/", (req, res) => {
-  res.send({ data: "hello world" });
 });
 
 const server = app.listen(process.env.port || 3001, () => {
